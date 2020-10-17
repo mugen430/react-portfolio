b52438eadc60bdc500aa805436a4fe09743839ec
Calvin <calvinchui382025@yahoo.com>
2020-10-17 03:48:15 -0500

    redirect to /landing if /

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import LandingPage from './landing';
 import AboutMe from './about';
 import Projects from './projects';
@@ -10,6 +10,11 @@ import Resume from './resume';
 const Main = () => {
     return (
         <Switch>
+            <Route
+                exact
+                path="/"
+                render={() => <Redirect to="/landing" />}
+            />
             <Route exact path="/landing" component={LandingPage} />
             <Route path="/about" component={AboutMe} />
             <Route path="/projects" component={Projects} />
